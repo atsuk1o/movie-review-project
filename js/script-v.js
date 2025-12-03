ce2dd6051cf22f49f1a7597ac146d1b46c3658cc
versurkova <veronica@yarmoshko.eu>
2025-12-03 21:57:41 +0000

    Add files via upload

--- js/script-v.js
+++ js/script-v.js
@@ -1,44 +1,46 @@
-function hover(){
-	const cards = document.querySelectorAll('.card-space');
-	cards.forEach(card => {
+/* JavaScript for The Expanse page */
+/* V Surkova */
+
+// Function to make cards a little bit bigger when hovered
+function hover() {
+  const cards = document.querySelectorAll('.card-space');
+
+  cards.forEach(card => {
     card.addEventListener('mouseover', () => {
       card.style.transform = 'scale(1.1)';
       card.style.transition = 'transform 0.3s';
     });
-
     card.addEventListener('mouseout', () => {
       card.style.transform = 'scale(1)';
     });
   });
 }
-hover();
 
+hover(); // run the hover function
+
+// Function to open card links in a new tab
 function newTab() {
   const cardLinks = document.querySelectorAll('.card .link');
-
   cardLinks.forEach(link => {
-    link.addEventListener('click', function(e) {
+    link.addEventListener('click', function (e) {
       e.preventDefault();
       window.open(this.href, '_blank');
     });
   });
 }
 
-newTab();
+newTab(); // run the newTab function
 
-function msg(){
-	alert("Thank you for letting us know! Your feedback is very important for us!");
-}
-
+// Function to check message form before sending
 function msg(event) {
-    const textarea = document.querySelector('form textarea');
-    if (textarea.value.trim() === "") {
-        alert("Please provide issue details before submitting!");
-		event.preventDefault();
-    } else {
-        alert("Thank you for letting us know! Your feedback is very important for us!");
-    }
+  const textarea = document.querySelector('form textarea');
+  if (textarea.value.trim() === "") {
+    alert("Please provide issue details before submitting!");
+    event.preventDefault();
+  } else {
+    alert("Thank you for letting us know! Your feedback is very important for us!");
+  }
 }
 
-const form = document.querySelector('form');
-form.addEventListener('submit', msg);
+const form = document.querySelector('form'); // get the form
+form.addEventListener('submit', msg); // check form when submitted
